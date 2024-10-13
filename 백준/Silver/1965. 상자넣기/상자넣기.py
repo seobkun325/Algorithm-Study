11c0b3d0ee147fb87e1be4ef071b174b64ec5be1
seobkun325 <165751207+seobkun325@users.noreply.github.com>
2024-10-13 15:42:59 +0900

    [Silver II] Title: 상자넣기, Time: 100 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+box = list(map(int, input().split(" ")))
+dp = [1]*n
+for i in range(1,n) :
+    hubo = [0]
+    for j in range(0, i) :
+        if box[j] < box[i] :
+            hubo.append(dp[j])
+    dp[i] = max(hubo)+1
+print(max(dp))
